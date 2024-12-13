5cceffb17173ea6a7a38a867d8b8bdfff8e73cc4
sandeep23256 <sandeepmpct23@gmail.com>
2024-12-13 18:24:07 +0530

    collegeportal

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const web = require('./routes/web')
 const connectDb = require('./database/connectDb')
+const session = require('express-session')
 
 
 
@@ -14,6 +15,18 @@ app.set('view engine', 'ejs')
 connectDb()
 //userinsert
 app.use(express.static('public'))
+//connect flash seasion
+const seasion =require('express-session')
+const flash =require('connect-flash')
+// massage
+app.use(session({
+    secret: 'secret',
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false,
+}));
+//flash massege
+app.use(flash());
 //route load
 app.use('/', web)
 //server create
